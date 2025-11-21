3d9389e0a2be50afa2f116c5401e41a93b8e56fc
Your Name <aryan.neshat25@students.bhasvic.ac.uk>
2025-11-21 14:28:22 +0000

    all done

--- main.c
+++ main.c
@@ -168,10 +168,8 @@ void checkin() {
             if ((adults + children) > 4) {
                 printf("Maximum guests allowed is 4.\n");
             }
-            if ((adults != 0 && adults != 1 && adults != 2 && adults != 3 && adults != 4) || (children != 0 && children != 1 && children != 2 && children != 3 && children != 4)) {
-                printf("Invalid format.\n");
-            }
-        }while (((adults != 0 && adults != 1 && adults != 2 && adults != 3 && adults != 4) && (children != 0 && children != 1 && children != 2 && children != 3 && children != 4)) || ((adults + children) > 4) || ((adults+children) < 1)||(children > 0 && adults < 1));
+        }while ((adults != 1 && adults != 2 && adults != 3 && adults != 4) || ( children != 1 && children != 2 && children != 3 && children != 4) || (adults + children) > 4 || (adults+children) < 1);
+
 
 
         int d= (confirmOrQuit("Is this correct?"));
